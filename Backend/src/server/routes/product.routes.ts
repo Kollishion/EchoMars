1f3341b774b0bc0edab16274201f2c3692498989
Abhishek <abhishek321bose@gmail.com>
2026-01-31 12:48:23 +0530

    update: built the modelSchema through zod and created a basic router for trpc

--- Backend/src/server/routes/product.routes.ts
+++ Backend/src/server/routes/product.routes.ts
@@ -1,10 +1,14 @@
 import { router, publicProcedure } from '../trpc.js';
-
+import { z } from 'zod';
+import { getAllProductsModel } from '../schemas/product.schema.js';
+import type { Product } from '../schemas/product.schema.js';
+const products: Product[] = [{ id: 12459, rating: { rate: 4, count: 4 }, productId: 1, title: 'Iphone 16', price: 1000, description: 'The iPhone 16 redefines performance and design with the A18 Bionic chip, brighter Super Retina XDR display, and enhanced battery life. Featuring advanced AI-powered photography, Dynamic Island, and iOS 18’s customizable interface, it’s Apple’s most intelligent and refined iPhone yet.', category: 'Smartphone', image: 'www.iphone.com' }];
 export const productRouter = router({
 	getAllProducts: publicProcedure
-		.input()
-		.output()
+		.input(z.undefined())
+		.output(getAllProductsModel)
 		.query(() => {
+			return products;
 
 		})
 });
--- Backend/src/server/schemas/product.schema.ts
+++ Backend/src/server/schemas/product.schema.ts
@@ -3,13 +3,17 @@ import { z } from "zod";
 export const productModel = z.object({
 	id: z.number().describe("UUID of the product id"),
 	rating: z.object({
-		rate: z.number().min(0).max(5).describe("Actual rating"),
+		rate: z.number().min(0).max(5).describe("Actual:q rating"),
 		count: z.number().describe("Stars count"),
 	}),
-	productId: z.number().describe("Connects "),
-	title: z.string().min(3),
-	price: z.number().nonnegative(),
-	description: z.string().min(3),
-	category: z.string().min(3),
-	image: z.string().url("Must be a valid URL"),
+	productId: z.number().describe("For cart"),
+	title: z.string().min(3).describe("Title of the product: Eg.: Iphone"),
+	price: z.number().nonnegative().describe("Price of the product in dollars"),
+	description: z.string().min(3).describe("Description of the product"),
+	category: z.string().min(3).describe("Particular Category of the product"),
+	image: z.string().url("Must be a valid URL").describe("Image URL for the product"),
 });
+
+export type Product = z.infer<typeof productModel>;
+
+export const getAllProductsModel = z.array(productModel);
